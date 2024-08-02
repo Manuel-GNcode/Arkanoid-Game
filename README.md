2313d9b0f8404a46d603889274ebe713031348ae
GNcode <gngalarga027@gmail.com>
2024-08-01 21:55:31 -0500

    Mejoré el sistema de coliciones y agregué los sprites a la nave y los ladrillos.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Arkanoid-Game
- Juego clásico de romper ladrillos.
+# Arkanoid Game in JavaScript
+
+This is a simple Arkanoid game created using HTML5 Canvas and JavaScript. The goal is to move the paddle to bounce a ball and destroy the bricks at the top. Have fun!
+
+## Instructions
+
+1. Clone this repository.
+2. Open the `index.html` file in your browser.
+3. Use the left and right arrow keys to move the paddle.
+4. Destroy all the bricks to win!
+
+## Technologies Used
+
+- HTML5 Canvas
+- JavaScript
+
+## Screenshot
+
+![alt text](src/screen-game.webp)
+
+## Credits
+
+This game was inspired by the classic Arkanoid by Taito.
+
+## Contributions
+
+If you'd like to contribute, feel free to do so! Open a pull request or create an issue.
+
+Enjoy playing! 🎮
+
--- script.js
+++ script.js
@@ -1,67 +1,249 @@
 const canvas = document.getElementById('canvas');
+const startBtn = document.getElementById('start');
+const resetBtn = document.getElementById('reset');
+const $lost = document.getElementById('lost');
+const $win = document.getElementById('win');
+
+const $paddle = document.getElementById('paddle');
+const $bricks = document.getElementById('bricks');
 
 canvas.width = 500;
-canvas.height = 500;
+canvas.height = 450;
 
 const ctx = canvas.getContext('2d');
 let leftPressed = false;
 let rightPressed = false;
 
-const ball = {
-    radius: 8,
-    x: (canvas.width/2 - 4),
-    y: (canvas.height - 30),
-    sx: 2,
-    sy: -2,
+let isPlaying = false;
+
+let speedBall = 3;
+let ball = {
+    radius: 4,
+    x: (canvas.width/2 - 2),
+    y: (canvas.height - 40 - 2),
+    sx: speedBall,
+    sy: -speedBall,
 }
-const board = {
-    w: 80,
-    h: 10,
-    x: (canvas.width/2 - 40),
-    y: (canvas.height - 30),
-    sx: 10,
+
+let board = {
+    w: 70,
+    h: 15,
+    x: (canvas.width/2 - 35),
+    y: (canvas.height - 20),
+    sx: 8,
 }
+const bricks = {
+    r: 5,
+    c: 9,
+    w: 50,
+    h: 20,
+    top: 25,
+    left: 25,
+}
+
+const generateLevel = ()=>{
+    let arr = [];
+    for (let col = 0; col < bricks.c; col++) {
+        arr[col] = [];
+        for (let row = 0; row < bricks.r; row++) {
+            const brickX = bricks.left + (col * (bricks.w));
+            const brickY = bricks.top + (row * (bricks.h));
+            const colorN = Math.floor(Math.random()*8);
+            arr[col][row] = {brickX, brickY, status: 1, color: colorN};
+        }
+    }
+    return arr;
+}
+let arrBricks = generateLevel();
 
 const drawBall = ()=>{
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = '#fff';
+    ctx.fillStyle = '#000';
     ctx.fill();
     ctx.closePath();
 }
 const drawPaddle = ()=>{
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(board.x, board.y, board.w, board.h);
+    ctx.drawImage(
+        $paddle,
+        0,
+        0,
+        88,
+        22,
+        board.x,
+        board.y,
+        board.w,
+        board.h
+    )
 }
 
-const drawBricks = ()=>{}
+const drawBricks = ()=>{
+    for (let col = 0; col < bricks.c; col++) {
+        for (let row = 0; row < bricks.r; row++) {
+            const currentBrick = arrBricks[col][row];
+            if (currentBrick.status == 0) continue;
+            ctx.drawImage(
+                $bricks,
+                (16*currentBrick.color),
+                0,
+                15.5,
+                7.5,
+                currentBrick.brickX,
+                currentBrick.brickY,
+                bricks.w,
+                bricks.h,
+            )
+        }
+    }
+}
 
-const collisionDetecton = ()=>{}
+const collisionDetecton = ()=>{
+    for (let col = 0; col < bricks.c; col++) {
+        for (let row = 0; row < bricks.r; row++) {
+            const currentBrick = arrBricks[col][row];
+            if (currentBrick.status == 0) continue;
+
+            const collisionXBrick = 
+            ball.x > currentBrick.brickX &&
+            ball.x < currentBrick.brickX + bricks.w;
+
+            const collisionYBrick = 
+            ball.y > currentBrick.brickY &&
+            ball.y < currentBrick.brickY + bricks.h;
+
+            const collisionLeftBrick = 
+            ball.x > currentBrick.brickX &&
+            ball.x < currentBrick.brickX + speedBall;
+            const collisionRightBrick = 
+            ball.x < currentBrick.brickX+bricks.w  &&
+            ball.x > currentBrick.brickX+bricks.w - speedBall;
+
+            const collisionTopBrick = 
+            ball.y > currentBrick.brickY &&
+            ball.y < currentBrick.brickY + speedBall;
+            const collisionBottomBrick = 
+            ball.y < currentBrick.brickY+bricks.h &&
+            ball.y > currentBrick.brickY+bricks.h - speedBall;
+
+            if (collisionXBrick && collisionYBrick) {
+                currentBrick.status = 0;
+                if (
+                    collisionLeftBrick || collisionRightBrick
+                ) {
+                    ball.sx = -ball.sx;
+                }
+                else if (
+                    collisionTopBrick || collisionBottomBrick
+                ) {
+                    ball.sy = -ball.sy;
+                }
+            }
+            
+        }
+    }
+}
 
 const ballMovement = ()=>{
     if (
-        ball.x + ball.sx + ball.radius > canvas.width
+        ball.x + ball.sx > canvas.width
         || ball.x + ball.sx < 0
     ) {
         ball.sx = -ball.sx;
     }
     if (ball.y + ball.sy < 0) {
         ball.sy = -ball.sy;
+    } else if (
+        ball.x > board.x &&
+        ball.x < board.x+board.w &&
+        ball.y > board.y &&
+        ball.y < board.y+board.h
+    ) {
+        ball.sy = -ball.sy
     }
-    //Game over
-    if (ball.y + ball.sy + ball.radius > canvas.height) {
-        // ball.x = (canvas.width/2 - 4);
-        // ball.y = (canvas.height - 30);
-        ball.sy = -ball.sy;
+    
+    if (ball.y-ball.radius > canvas.height) {
+        $lost.style.display = 'block';
+        isPlaying = false;
     }
-    ball.x += ball.sx;
-    ball.y += ball.sy;
+    if (isPlaying) {
+        ball.x += ball.sx;
+        ball.y += ball.sy;
+    }
 }
 
-const paddleMovement = ()=>{}
+const paddleMovement = ()=>{
+    if (board.x < 0) {
+        leftPressed = false;
+    }
+    if (board.x+board.w > canvas.width) {
+        rightPressed = false;
+    }
+    if (rightPressed) {
+        board.x += board.sx;
+    } else if (leftPressed) {
+        board.x -= board.sx;
+    }
+}
 
 const clearCanvas = ()=>{
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+const initEvents = ()=>{
+    document.addEventListener('keydown', (e)=>{
+        if (e.key == 'ArrowRight' || e.key == 'Right') {
+            rightPressed = true;
+        } else if (e.key == 'ArrowLeft' || e.key == 'Left') {
+            leftPressed = true;
+        }
+    });
+    document.addEventListener('keyup', (e)=>{
+        if (e.key == 'ArrowRight' || e.key == 'Right') {
+            rightPressed = false;
+        } else if (e.key == 'ArrowLeft' || e.key == 'Left') {
+            leftPressed = false;
+        }
+    })
+}
+const resetGame = ()=>{
+    isPlaying = false;
+    ball = {
+        radius: 4,
+        x: (canvas.width/2 - 2),
+        y: (canvas.height - 40 - 2),
+        sx: speedBall,
+        sy: -speedBall,
+    }
+    board = {
+        w: 70,
+        h: 15,
+        x: (canvas.width/2 - 35),
+        y: (canvas.height - 20),
+        sx: 8,
+    }
+
+    $lost.style.display = 'none';
+    $win.style.display = 'none';
+
+    arrBricks = generateLevel();
+    draw();
+}
+const winGame = ()=>{
+    let isWin = true;
+    for (let col = 0; col < bricks.c; col++) {
+        for (let row = 0; row < bricks.r; row++) {
+            const currentBrick = arrBricks[col][row];
+            if (currentBrick.status == 1) {
+                isWin = false;
+                break;
+            };
+
+        }
+    }
+    if (isWin) {
+        $win.style.display = 'block';
+        isPlaying = false;
+        console.log('2')
+    }
 }
 
 const draw = ()=>{
@@ -69,11 +251,19 @@ const draw = ()=>{
     drawBall();
     drawPaddle();
     drawBricks();
+    winGame();
 
     collisionDetecton();
     ballMovement();
     paddleMovement();
 
-    window.requestAnimationFrame(draw);
+    if (isPlaying) window.requestAnimationFrame(draw);
 }
-draw()
+draw();
+initEvents();
+
+startBtn.addEventListener('click', ()=>{
+    isPlaying = true;
+    draw();
+})
+resetBtn.addEventListener('click', resetGame);
